7891ba3db31f4439001f4b07dd0ffe2ef0bbf7d4
pponssb <pau.ponsbo@gmail.com>
2025-06-18 20:18:49 +0200

    Bot jugadors
    He afegit botons per triar num jugadors

--- Botons_Jugadors.gd
+++ Botons_Jugadors.gd
@@ -1,1 +1,10 @@
 extends Control
+@onready var botons_container = $Bot_jugadors
+func _ready():
+	for i in range(1,5):
+		var boto = Button.new()
+		boto.text=str(i)+"jugador"+("s" if i>1 else "")
+		boto.pressed.connect(func(): seleccionar_jugadors(i))
+		botons_container.add_child(boto)
+func seleccionar_jugadors(num):
+	print("El joc procedirà amb",num, "jugador(s)")
