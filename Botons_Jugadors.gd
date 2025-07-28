e92e08c4890557fa825ba2277aa9924e37eb5d81
pponssb <pau.ponsbo@gmail.com>
2025-07-28 18:48:50 +0200

    Botons principi
    Marc he fet la part del principi, quan executes el programa et deixa triar el nombre de jugadors i si apretes el botó de començar ja passa a la escena principal

--- Botons_Jugadors.gd
+++ Botons_Jugadors.gd
@@ -1,10 +1,21 @@
 extends Control
+
 @onready var botons_container = $Bot_jugadors
+@onready var njt = $Num_jug_txt
+
 func _ready():
-	for i in range(1,5):
+	for i in range(2, 6):
 		var boto = Button.new()
-		boto.text=str(i)+"jugador"+("s" if i>1 else "")
+		boto.text = str(i) + " jugador" + ("s" if i > 1 else "")
 		boto.pressed.connect(func(): seleccionar_jugadors(i))
 		botons_container.add_child(boto)
+
 func seleccionar_jugadors(num):
-	print("El joc procedirà amb",num, "jugador(s)")
+	njt.text = "El joc procedirà amb " + str(num) + " jugadors"
+	njt.show()
+	for boto in botons_container.get_children():
+		botons_container.remove_child(boto)
+		boto.queue_free()
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
